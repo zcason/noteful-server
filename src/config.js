1a12ed7afeba0f36759c21842c9506592a1eb8d3
zcason <zuri.cason@gmail.com>
2020-11-29 16:01:39 -0600

    deployment check

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:password@localhost/noteful',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test'
 }
